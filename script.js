be1a71be301e0c593018997b3abaf223104b2028
dycreatorx-arch <dycreatorx@gmail.com>
2026-02-03 14:22:12 -0800

    Refactor script.js for future expansion
    Removed button click event listener and added a log message.

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-document.getElementById('clickBtn').addEventListener('click', function() {
-    alert('Hello! You clicked the button!');
-});
+// Simple JS for future expansion
+console.log("TrainingHub loaded successfully");
